751559787216dc55d7321ab6c7471037317d410a
kkalina <kkalina@umich.edu>
2016-10-23 23:39:56 -0400

    first commit

---
+++ Assets/_Scripts/steering.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class steering : MonoBehaviour {
+
+    public float steeringPos = 0;
+    public float travel = 0;
+    public float cosmeticPos = 0;
+    public float cosmeticTravel = 0.1f;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        steeringPos = Input.GetAxis("Horizontal")*travel;
+        cosmeticPos = Input.GetAxis("Horizontal") * cosmeticTravel;
+        this.transform.localRotation = Quaternion.EulerAngles(0, cosmeticPos, 0);
+        this.GetComponent<WheelCollider>().steerAngle = steeringPos;
+
+        
+	}
+}
---
+++ Assets/playerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class playerController : MonoBehaviour {
+
+    public float speed = 5f;
+    /*
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        this.gameObject.GetComponent<Rigidbody>().AddForce(this.transform.forward*-1f*Input.GetAxis("Vertical")*speed);
+	}*/
+
+    public List<AxleInfo> axleInfos; // the information about each individual axle
+    public float maxMotorTorque; // maximum torque the motor can apply to wheel
+    public float maxSteeringAngle; // maximum steer angle the wheel can have
+
+    public void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor; // is this wheel attached to motor?
+    public bool steering; // does this wheel apply steer angle?
+}
+
